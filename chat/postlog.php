1d394415b7e6484ef2151d8976e2f089ffddf220
ThomasB <thomas.bellettini@epitech.eu>
2021-03-29 03:58:50 +0200

    Push For Guest Rank

--- chat/postlog.php
+++ chat/postlog.php
@@ -14,7 +14,7 @@ if(isset($_SESSION['name'])) {
     } else if ($_SESSION['rank'] == 2) {
         fwrite($fp, "<div class='msgln'>(" . strftime('%H:%M:%S') . ") <span class=\"badge rounded-pill bg-danger\">Administrateur</span> <b>" . $_SESSION['name'] . " </b>: " . stripslashes(htmlspecialchars($text)) . "<br></div>");
     } else {
-        fwrite($fp, "<div class='msgln'>(".strftime('%H:%M:%S').") <span class=\"badge rounded-pill bg-warning text-dark\">Warning</span> <b>".$_SESSION['name']." </b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+        fwrite($fp, "<div class='msgln'>(".strftime('%H:%M:%S').") <span class=\"badge rounded-pill bg-warning text-dark\">Guest</span> <b>".$_SESSION['name']." </b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
     }
     fclose($fp);
 }
